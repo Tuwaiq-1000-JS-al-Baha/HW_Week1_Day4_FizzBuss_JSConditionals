e4bccfefb9243f56a620a14e4b1f85af2218d646
SuaadAlgamdi <Suaad.Abduallh.abt.js@tuwaiq.edu.sa>
2021-10-13 20:54:00 +0300

    hw day4 done .think you

---
+++ hwvarible.js
@@ -0,0 +1,24 @@
+let number=76848
+
+if (number*3) {
+console.log("FIZZ") 
+ {
+    
+}
+
+if (number*5); 
+    console.log("BUZZ")
+    
+}
+else if (number*3&&number*5){
+
+
+   console.log(FIZZBUZZ) 
+};
+if(number=number);{
+    console.log(number)
+}
+  
+
+
+
