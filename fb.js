7bc22e523ab57aa8232ea4d4f49bbc03b555705a
Magdi7021 <92311360+Magdi7021@users.noreply.github.com>
2021-10-13 18:37:04 +0300

    fb.js

---
+++ fb.js
@@ -0,0 +1,16 @@
+var num1 = 15
+if (num1 % 3 == 0 && num1 % 5 == 0) {
+console.log("FuzzBuzz")
+}
+else if (num1 % 3 == 0){
+
+console.log("fizz")
+}
+else if (num1 % 5 == 0){
+
+ console.log("buzz")   
+}
+
+else {
+    console.log(num1)
+}
