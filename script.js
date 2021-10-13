75f3c94d19272e873e009521726f98f574877778
Wa2999 <92243050+Wa2999@users.noreply.github.com>
2021-10-13 20:54:17 +0300

    HOMEWORK4

---
+++ script.js
@@ -0,0 +1,14 @@
+let number = 30
+if (number % 5 == 0 && number % 3 == 0) {
+    console.log("FIZZ BUZZ")
+}
+else if (number % 3 == 0) {
+    console.log("FIZZ")
+}
+else if (number % 5 == 0) {
+    console.log("BUZZ")
+}
+
+else {
+    console.log(number)
+}
