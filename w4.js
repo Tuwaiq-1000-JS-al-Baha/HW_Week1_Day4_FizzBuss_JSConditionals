15657fc62ff0bf757f4f33033578a52c3a020063
Meshoeternity <Mashael.othman.abt.js@tuwaiq.edu.sa>
2021-10-13 19:03:17 +0300

    done hw4

---
+++ w4.js
@@ -0,0 +1,11 @@
+let number =25
+if (number%3 == 0){
+    console .log ("FIZZ")}
+else if(number%5 ==0)
+    console.log("BUZZ")
+else if(number% 3==0 && number%5 ==0)
+    console.log ("FIZZBUZZ")
+    else { console .log (number)}
+
+
+
