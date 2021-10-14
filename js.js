45ec76fe5506c3e7c8a76d1deb74f716cb3731ca
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-14 16:12:25 +0300

    send slack but send again

---
+++ js.js
@@ -0,0 +1,14 @@
+let num = 3
+
+ if (num % 3 === 0 && num % 5 === 0) {  
+       console.log('FizzBuzz')
+
+    }else if (num % 5 === 0) { 
+           console.log('bizz')
+
+        } else if (num % 3 === 0) {   
+             console.log('fuzz')
+
+        }  else { 
+               console.log(num)
+        }
